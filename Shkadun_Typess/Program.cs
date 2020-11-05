26b4b19233a22a6946a3821ee94117be83ba7fa0
rbisb <rbisb@DESKTOP-MM26GKA>
2020-11-05 13:24:53 +0300

    Добавьте файлы проекта.

---
+++ Shkadun_Typess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shkadun_Typess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
